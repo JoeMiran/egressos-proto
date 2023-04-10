5894c0778f063dea61662aa319e4c0f7e7b43bc7
TacioDaito <taciokikuchi@gmail.com>
2023-04-10 13:06:42 -0300

    atualização  formularioEgresso.php

--- formularioEgresso.php
+++ formularioEgresso.php
@@ -61,8 +61,9 @@ definirDadosSessao();
 		
 	</fieldset><br>
 	
+	
 	<fieldset>
-		
+	
 		<legend>Dados Complementares</legend><br>
 
 		<div>
@@ -94,7 +95,9 @@ definirDadosSessao();
 
 		</div>
 		<br><br>
-
+	</fieldset><br>
+	
+	<fieldset>
 		<div>
 			<p>Durante o curso, você desenvolveu atividade remunerada fora da Universidade?</p>
 
@@ -137,10 +140,139 @@ definirDadosSessao();
 
 		</div>
 		<br><br>
+	</fieldset><br>
 
+	<fieldset>
 
+		<a>Em caso de Curso PÃ³s-GraduaÃ§Ã£o, realizado ou andamento, Ã© na UFPA?</a>
+		<select id="localPos" name="localPos">
+				<option>Selecione</option>
+				<option value="sim">Sim</option>
+				<option value="nao">NÃ£o</option>
+		</select><br><br>
 
 	</fieldset><br>
+
+	<fieldset>
+
+		<legend>SituaÃ§Ã£o Profissional Atual</legend>
+		<label for="mercado">Atualmente estÃ¡ inserido/a no mercado de trabalho?</label>
+		<select id="mercado" name="mercado">
+			<option>Selecione</option>
+			<option value="sim">Sim</option>
+			<option value="nao">NÃ£o</option>
+		</select><br><br>
+		<a>Se estÃ¡, qual a forma de obtenÃ§Ã£o de emprego?</a><br>
+		<input type="checkbox" id="consurso" name="concurso">
+		<label for="concurso">Consurso</label><br>
+		<input type="checkbox" id="ctps" name="ctps">
+		<label for="ctps">CTPS</label><br>
+		<input type="checkbox" id="autonomo" name="autonomo">
+		<label for="autonomo">AutÃ´nomo</label><br>
+		<input type="checkbox" id="informal" name="informal">
+		<label for="informal">Atividade informal</label><br>
+
+	</fieldset><br>
+
+	<fieldset>
+
+		<a>Tempo necessário para conseguir atividade remunerada após a formatura:</a>
+		<div>
+			<input type=radio id="poucotempoMercado" name="tempoMercado">
+			<label for="poucotempoMercado">0 a 6 meses</label><br>
+			<input type=radio id="mediotempoMercado" name="tempoMercado">
+			<label for="mediotempoMercado">7 a 12 meses</label><br>
+			<input type=radio id="maiortempoMercado" name="tempoMercado">
+			<label for="maiortempoMercado">mais de 12 meses</label><br>
+		</div>
+
+	</fieldset><br>
+
+	<fieldset>
+		
+		<a>Seu trabalho é na sua área de formação?</a>
+		<select id="saida" name="saida">
+			<option>Selecione</option>
+			<option value="sim">Sim</option>
+			<option value="nao">Não</option>
+		</select>
+
+	</fieldset><br>
+
+	<fieldset>
+		
+		<a>Faixa salarial:</a>
+		<div>
+			<input type=radio id="baixoSalario" name="salario">
+			<label for="baixoSalario">1 a 2 salários mínimos</label><br>
+			<input type=radio id="medioSalario" name="salario">
+			<label for="medioSalario">3 a 5 salários mínimos</label><br>
+			<input type=radio id="altoSalario" name="salario">
+			<label for="altoSalario">mais de 5 salários</label><br>
+		</div>
+
+	</fieldset><br>
+
+	<fieldset>
+
+		<legend>Atividade Laboral</legend>
+
+		<a>Há uma relação entre o curso de graduação concluido e a atividade laboral que está exercendo?</a><br>
+		<div>
+			<input type="radio" id="simTrabalho" name="formacaoTrabalho">
+			<label>Sim</label><br>
+			<input type="radio" id="naoTrabalho" name="formacaoTrabalho">
+			<label>Não</label>
+		</div><br>
+
+		<a>Recebeu orientação, no âmbito do seu respectivo curso, para atuar no mercado de trabalho?</a><br>
+		<div>
+			<input type="radio" id="comOrientacao" name="orientacao">
+			<label for="comOrientacao">Sim</label><br>
+			<input type="radio" id="naoTrabalho" name="orientacao">
+			<label for="semOrientacao">Não</label>
+		</div><br>
+
+		<a>As temáticas e/ou assuntos abordados nas várias disciplinas cursadas tiveram utilidade para o exercício profissional?</a>
+		<div>
+			<input type="radio" id="util" name="utilidade">
+			<label for="util">Sim</label><br>
+			<input type="radio" id="inutil" name="utilidade">
+			<label for="inutil">Não</label>
+		</div><br>
+
+		<a>O(s) estágio(s) que cumpriu no curso contribuiu para a inserção profissional no mercado de trabalho?</a>
+		<div>
+			<input type="radio" id="contribuiu" name="contribuicao">
+			<label for="contribuiu">Sim</label><br>
+			<input type="radio" id="naoContribuiu" name="contribuicao">
+			<label for="naoContribuiu">Não</label><br>
+			<input type="radio" id="indiferente" name="contribuicao">
+			<label for="indiferente">Não fiz estágio</label>
+		</div><br>
+
+		<label>Comente:</label><br>
+		<textarea placeholder="Descreva mais um pouco como anda sua situação profissional..."></textarea><br>
+
+	</fieldset><br>
+
+	<fieldset>
+
+		<legend>Nível de Satisfação</legend>
+
+		<a>Em relação à satisfação com seu curso, você está?</a>
+		<div>
+			<input type="radio" id="muitoSatisfeito" name="satisfacao">
+			<label for="muitoSatisfeito">Muito Satisfeito</label><br>
+			<input type="radio" id="satisfeito" name="satisfacao">
+			<label for="satisfeito">Satisfeito</label><br>
+			<input type="radio" id="insatisfeito" name="satisfacao">
+			<label for="insatisfeito">Insatisfeito</label>
+		</div><br>
+
+	</fieldset>
+
+
 
 	<input type="submit" name="botaoConfirmar" id="botaoConfirmar" 
 		value="Enviar Resposta">
