0569c14b8f3e9550c5ab0bdb31cf62461f5a8edf
israelsilvaa <Israel524.is@gmail.com>
2023-04-18 09:29:31 -0300

    18042023a: consulta das respostas completa (php)

--- consultaResposta.php
+++ consultaResposta.php
@@ -2,37 +2,46 @@
 
 include_once 'dadosEgresso.php';
 session_start();
-
 $dados = getDadosEgressoFromDatabase($_SESSION['cpf']);
 
-echo 'CPF: ' . $dados['cpf'] . '<br><br>';
-
-echo 'Nome: ' . $dados['nome'] . '<br><br>';
-
-echo 'Data de Nascimento: ' . $dados['dataNascimento'] . '<br><br>';
-
-echo 'Email: ' . $dados['email'] . '<br><br>';
-
-echo 'Curso: ' . $dados['curso'] . '<br><br>';
-
-echo 'Ano de Ingresso: ' . $dados['anoIngresso'] . '<br><br>';
-
-echo 'Campus: ' . $dados['campus'] . '<br><br>';
-
-echo 'Respostas: <br><br>';
-
-echo 'Genero: ' . $dados['genero'] . '<br><br>';
-
-echo 'Cor: ' . $dados['cor'] . '<br><br>';
-
-echo 'Idade: ' . $dados['idade'] . '<br><br>';
-
-echo 'Obteve bolsa durante o curso? <br>' . $dados['bolsa'] . '<br><br>';
-
-echo 'Se sim, qual(is)? <br>' . $dados['resumoBolsa'] . '<br><br>';
-
-echo 'Participou de atividades acadêmicas extracurriculares durante o curso?<br><br>';
-
-echo 'Atividades Extracurriculares: <br>' . $dados['atividadesExtracurriculares'] . '<br><br>';
-
 ?>
+
+<?php echo $dados['cpf'];?>
+<?php echo $dados['nome'];?>
+<?php echo $dados['email'];?>
+<?php echo $dados['dataNascimento'];?>
+<?php echo $dados['idade'];?>
+<?php echo $dados['genero'];?>
+<?php echo $dados['cor'];?>
+<?php echo $dados['curso'];?>
+<?php echo $dados['anoIngresso'];?>
+<?php echo $dados['campus'];?>
+<?php echo $dados['bolsa'];?>
+<?php echo $dados['resumoBolsa'];?>
+<?php echo $dados['atividadesExtracurriculares'];?>
+<?php echo $dados['atividadeRemunerada'];?>
+<?php echo $dados['cursoPosGraduacao'];?>
+<?php echo $dados['situacaoCursoPosGraduacao'];?>
+<?php echo $dados['posGraduacaoUfpa'];?>
+<?php echo $dados['inseridoNoMercado'];?>
+<?php echo $dados['tipoDeEmprego'];?>
+<?php echo $dados['tempoFormaturaEmprego'];?>
+<?php echo $dados['trabalhaNaAreaDeFormacao'];?>
+<?php echo $dados['faixaSalarial'];?>
+<?php echo $dados['relacaoCursoTrabalho'];?>
+<?php echo $dados['recebeuOrientacao'];?>
+<?php echo $dados['disciplinasForamUteis'];?>
+<?php echo $dados['estagioContribuiuEmprego'];?>
+<?php echo $dados['resumoSituacaoProfissional'];?>
+<?php echo $dados['satisfacaoComCurso'];?>
+<?php echo $dados['resumoSatisfacaoComCurso'];?>
+<?php echo $dados['recomendaCurso'];?>
+<?php echo $dados['resumoRecomendacaoCurso'];?>
+<?php echo $dados['participaDeEventos'];?>
+<?php echo $dados['resumoEventosAtuais'];?>
+<?php echo $dados['participaDeProjeto'];?>
+<?php echo $dados['resumoProjetosAtuais'];?>
+<?php echo $dados['participaDeCurso'];?>
+<?php echo $dados['resumoCursosAtuais'];?>
+<?php echo $dados['resumoAtividadesInteresse'];?>
+<?php echo $dados['resumoSugestaoQuestionario'];?>
